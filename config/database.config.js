9af96c4fd07ccdd0fdcaab63d4c48f56e9f1eb05
HaThaoLy <lekietct95@gmail.com>
2024-11-19 08:43:35 +0700

    second hh

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://lekietct95:<hoangphuc1203>@cluster0.8jnci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://lekietct95:hoangphuc1203@cluster0.8jnci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
